b1295919fdeb767e5ccdc37ca8f5870af4a7f176
Rasdhan123 <147924983+Rasdhan123@users.noreply.github.com>
2023-10-14 13:43:22 +0100

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,58 @@
+<?php
+
+$fname = $_POST['fname'];
+$lname  = $_POST['lname'];
+$emailid = $_POST['emailid'];
+$epassword = $_POST['epassword'];
+$copassword = $_POST['copassword'];
+
+
+
+
+if (!empty($fname) || !empty($lname) ||  !empty($emailid) || !empty($epassword) || !empty($copassword) )
+{
+
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "register";
+
+
+
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+if (mysqli_connect_error()){
+  die('Connect Error ('. mysqli_connect_errno() .') '
+    . mysqli_connect_error());
+}
+else{
+  $SELECT = "SELECT emailid From account Where emailid = ? Limit 1";
+  $INSERT = "INSERT Into account (fname , lname, emailid, epassword, copassword )values(?,?,?,?)";
+
+//Prepare statement
+     $stmt = $conn->prepare($SELECT);
+     $stmt->bind_param("s", $emailid);
+     $stmt->execute();
+     $stmt->bind_result($emailid);
+     $stmt->store_result();
+     $rnum = $stmt->num_rows;
+
+     //checking username
+      if ($rnum==0) {
+      $stmt->close();
+      $stmt = $conn->prepare($INSERT);
+      $stmt->bind_param("ssss", $fname,$lname,$emailid,$epassword,$copassword);
+      $stmt->execute();
+      echo "New record inserted sucessfully";
+     } else {
+      echo "Someone already register using this email";
+     }
+     $stmt->close();
+     $conn->close();
+    }
+} else {
+ echo "All field are required";
+ die();
+}
+?>
